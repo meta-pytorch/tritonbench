dc52e28b3702229091a936906f87044117586c25
Xu Zhao <xzhao9@fb.com>
2025-09-19 11:44:48 -0700

    format

--- tritonbench/components/power/chart.py
+++ tritonbench/components/power/chart.py
@@ -1,12 +1,12 @@
-import logging
-import subprocess
-import signal
-import torch
-import time
-import os
-
 import csv
+import logging
+import os
+import signal
+import subprocess
+import time
+
 import matplotlib.pyplot as plt
+import torch
 
 # query every 10 ms
 QUERY_FREQUENCY = 10
@@ -22,11 +22,13 @@ POWER_OUTPUT_DIR = None
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
 def _get_cuda_device_id():
     return torch.cuda.current_device()
 
+
 def _gen_power_charts(benchmark_name: str, device_name: str, power_csv_file: str):
-     # Read CSV
+    # Read CSV
     with open(power_csv_file) as f:
         reader = csv.reader(f)
         header = next(reader)  # first row as header
@@ -36,10 +38,20 @@ def _gen_power_charts(benchmark_name: str, device_name: str, power_csv_file: str
         for row in reader:
             for col, value in zip(header, row):
                 if value == "[N/A]":
-                    logger.warning(f"[tritonbench][power] {col} is not available, skipping")
+                    logger.warning(
+                        f"[tritonbench][power] {col} is not available, skipping"
+                    )
                     value = 0.0
                 else:
-                    value = float(value) if col not in ["clocks_event_reasons.hw_thermal_slowdown", "clocks_event_reasons.sw_thermal_slowdown"] else value
+                    value = (
+                        float(value)
+                        if col
+                        not in [
+                            "clocks_event_reasons.hw_thermal_slowdown",
+                            "clocks_event_reasons.sw_thermal_slowdown",
+                        ]
+                        else value
+                    )
                 data[col].append(value)
 
     # Generate synthetic time axis (100 ms per sample)
@@ -53,8 +65,12 @@ def _gen_power_charts(benchmark_name: str, device_name: str, power_csv_file: str
     plt.xlabel("Time (s)")
     plt.ylabel("Power (W)")
     plt.legend()
-    plt.title(f"[tritonbench] {benchmark_name} power consumption over time on {device_name}")
-    plt.savefig(os.path.join(POWER_OUTPUT_DIR, "power.png"), dpi=300, bbox_inches="tight")
+    plt.title(
+        f"[tritonbench] {benchmark_name} power consumption over time on {device_name}"
+    )
+    plt.savefig(
+        os.path.join(POWER_OUTPUT_DIR, "power.png"), dpi=300, bbox_inches="tight"
+    )
     # Plot temp chart
     plt.figure(figsize=(10, 6))
     for temp_col in header[3:5]:
@@ -63,7 +79,9 @@ def _gen_power_charts(benchmark_name: str, device_name: str, power_csv_file: str
         plt.ylabel("Temperature (C)")
     plt.legend()
     plt.title(f"[tritonbench] {benchmark_name} temperature over time on {device_name}")
-    plt.savefig(os.path.join(POWER_OUTPUT_DIR, "temp.png"), dpi=300, bbox_inches="tight")
+    plt.savefig(
+        os.path.join(POWER_OUTPUT_DIR, "temp.png"), dpi=300, bbox_inches="tight"
+    )
     # Plot frequency chart
     plt.figure(figsize=(10, 6))
     for temp_col in header[5:7]:
@@ -72,11 +90,13 @@ def _gen_power_charts(benchmark_name: str, device_name: str, power_csv_file: str
         plt.ylabel("Frequency (MHz)")
     plt.legend()
     plt.title(f"[tritonbench] {benchmark_name} frequency over time on {device_name}")
-    plt.savefig(os.path.join(POWER_OUTPUT_DIR, "freq.png"), dpi=300, bbox_inches="tight")
+    plt.savefig(
+        os.path.join(POWER_OUTPUT_DIR, "freq.png"), dpi=300, bbox_inches="tight"
+    )
 
 
 def power_chart_begin(benchmark_name, output_dir):
-    # check no other proc is running 
+    # check no other proc is running
     global QUERY_PROC, POWER_OUTPUT_DIR
     assert QUERY_PROC is None, "Power query process must be None to start a new one"
     # clean up the directory
@@ -84,12 +104,17 @@ def power_chart_begin(benchmark_name, output_dir):
     if not os.path.exists(POWER_OUTPUT_DIR):
         os.mkdir(POWER_OUTPUT_DIR)
     stdout_file_path = os.path.join(POWER_OUTPUT_DIR, QUERY_STDOUT_FILE)
-    stderr_file_path = os.path.join(POWER_OUTPUT_DIR, QUERY_STDERR_FILE) 
+    stderr_file_path = os.path.join(POWER_OUTPUT_DIR, QUERY_STDERR_FILE)
     # Run the command
-    query_cmd = QUERY_COMMAND.format(QUERY_FREQUENCY=QUERY_FREQUENCY, QUERY_DEVICE=_get_cuda_device_id()).split(" ")
-    with open(stdout_file_path, "w") as stdout_file, open(stderr_file_path, "w") as stderr_file:
-        QUERY_PROC = subprocess.Popen(query_cmd, stdout=stdout_file, stderr=stderr_file, start_new_session=True)
-
+    query_cmd = QUERY_COMMAND.format(
+        QUERY_FREQUENCY=QUERY_FREQUENCY, QUERY_DEVICE=_get_cuda_device_id()
+    ).split(" ")
+    with open(stdout_file_path, "w") as stdout_file, open(
+        stderr_file_path, "w"
+    ) as stderr_file:
+        QUERY_PROC = subprocess.Popen(
+            query_cmd, stdout=stdout_file, stderr=stderr_file, start_new_session=True
+        )
 
 
 def power_chart_end():
@@ -98,7 +123,9 @@ def power_chart_end():
     # Kill the process
     QUERY_PROC.send_signal(signal.SIGINT)
     time.sleep(0.2)
-    assert QUERY_PROC.poll() is not None, "Power query process must be killed to proceed"
+    assert (
+        QUERY_PROC.poll() is not None
+    ), "Power query process must be killed to proceed"
     # generate the chart based on csv
     stdout_file_path = os.path.join(POWER_OUTPUT_DIR, QUERY_STDOUT_FILE)
     benchmark_name = os.path.basename(POWER_OUTPUT_DIR)
