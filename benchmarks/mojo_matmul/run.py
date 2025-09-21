2c309d9d6934e8dc4e9b08b13b93cb7814d3d92a
Xu Zhao <xzhao9@fb.com>
2025-09-21 06:55:58 -0700

    add

--- benchmarks/mojo_matmul/run.py
+++ benchmarks/mojo_matmul/run.py
@@ -101,7 +101,7 @@ def mojo_matmul(operator, a, b, bias) -> Callable:
     return output_func
 
 if __name__ == "__main__":
-    args = ["--op", "gemm", "--only", "aten_matmul,mojo_matmul", "--precision", "bf16", "--m", "512", "--n", "8192", "--k", "5376"]
+    args = ["--op", "gemm", "--only", "aten_matmul,mojo_matmul", "--precision", "bf16", "--m", "512", "--n", "8192", "--k", "5376"] + sys.argv[1:]
     gemm_opbench_cls = load_opbench_by_name("gemm")
     parser = get_parser(args)
     tb_args, extra_args = parser.parse_known_args(args)
