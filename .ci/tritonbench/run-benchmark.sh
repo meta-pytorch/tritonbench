c846e79330171f439db39940a351d419c2d717bd
Xu Zhao <i@xuzhao.net>
2025-10-01 20:33:42 -0400

    fix git fail

--- .ci/tritonbench/run-benchmark.sh
+++ .ci/tritonbench/run-benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -xeuo pipefail
 
 if [ -z "${SETUP_SCRIPT}" ]; then
   echo "ERROR: SETUP_SCRIPT is not set"
@@ -32,6 +32,7 @@ tritonbench_dir=$(dirname "$(readlink -f "$0")")/../..
 cd "${tritonbench_dir}"
 
 # check if the current repo has "dubious ownership" issue
+git config --global --add safe.directory "${tritonbench_dir}"
 git rev-parse --verify HEAD
 
 echo "Running ${BENCHMARK_NAME} benchmark under conda env ${CONDA_ENV}"
