5efe5f258d8ec12d887d721e66fcaa79806047cf
Xu Zhao <xzhao9@meta.com>
2025-10-16 10:32:57 -0400

    fix helion

--- tools/helion/install.py
+++ tools/helion/install.py
@@ -11,12 +11,12 @@ HEION_COMMIT = "1aaba3f33fcbd730ce24a13bcd76d49e0f536ede"
 
 def install_helion():
     HELION_INSTALL_PATH.mkdir(parents=True, exist_ok=True)
-    git_clone_cmd = ["git", "clone", "https://github.com/pytorch/helion.git", cwd=HELION_INSTALL_PATH]
-    subprocess.check_call(git_clone_cmd)
+    git_clone_cmd = ["git", "clone", "https://github.com/pytorch/helion.git"]
+    subprocess.check_call(git_clone_cmd, cwd=HELION_INSTALL_PATH)
     HELION_PATH = HELION_INSTALL_PATH.joinpath("helion")
-    git_checkout_cmd = ["git", "checkout", HEION_COMMIT, cwd=HELION_PATH]
-    subprocess.check_call(git_clone_cmd)
-    install_requirements_cmd = ["pip", "install", "-r", "requirements.txt", cwd=HELION_PATH]
-    subprocess.check_call(install_requirements_cmd)
-    install_helion_cmd = ["pip", "install", "-e", ".'[dev]'", cwd=HELION_PATH]
-    subprocess.check_call(install_helion_cmd)
+    git_checkout_cmd = ["git", "checkout", HEION_COMMIT]
+    subprocess.check_call(git_clone_cmd, cwd=HELION_PATH)
+    install_requirements_cmd = ["pip", "install", "-r", "requirements.txt", ]
+    subprocess.check_call(install_requirements_cmd, cwd=HELION_PATH)
+    install_helion_cmd = ["pip", "install", "-e", ".'[dev]'"]
+    subprocess.check_call(install_helion_cmd, cwd=HELION_PATH)
