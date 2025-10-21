59cc2371e1277a65697a260a99b8477340affb9e
Xu Zhao <xzhao9@fb.com>
2025-10-20 17:53:52 -0700

    add power analysis

--- benchmarks/power_analysis/run.py
+++ benchmarks/power_analysis/run.py
@@ -3,6 +3,9 @@ Perform power and performance analysis on a Triton kernel.
 """
 
 import argparse
+import logging
+import os
+import sys
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,21 +14,23 @@ logging.basicConfig(level=logging.INFO)
 
 
 def setup_tritonbench_cwd():
-    original_dir = abspath(os.getcwd())
+    original_dir = os.path.abspath(os.getcwd())
 
     for tritonbench_dir in (
         ".",
         "../../../tritonbench",
     ):
-        if exists(tritonbench_dir):
+        if os.path.exists(tritonbench_dir):
             break
 
-    if exists(tritonbench_dir):
-        tritonbench_dir = abspath(tritonbench_dir)
+    if os.path.exists(tritonbench_dir):
+        tritonbench_dir = os.path.abspath(tritonbench_dir)
         os.chdir(tritonbench_dir)
         sys.path.append(tritonbench_dir)
     return original_dir
 
+
+setup_tritonbench_cwd()
 
 from tritonbench.utils.run_utils import load_operator_by_args
 
