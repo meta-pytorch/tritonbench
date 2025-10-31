005b00f0ecb8df9d4963f9265192662bbeee8655
Xu Zhao <xzhao9@fb.com>
2025-10-31 07:28:05 -0700

    simplify

--- tritonbench/operators/Mlp/operator.py
+++ tritonbench/operators/Mlp/operator.py
@@ -36,18 +36,18 @@ class Mlp(nn.Module):
 
         self.fc1 = linear_layer(in_features, hidden_features, bias=bias[0])
         self.act = act_layer()
-        self.drop1 = nn.Dropout(drop_probs[0])
-        self.norm = norm_layer(hidden_features) if norm_layer is not None else nn.Identity()
-        self.fc2 = linear_layer(hidden_features, out_features, bias=bias[1])
-        self.drop2 = nn.Dropout(drop_probs[1])
+        # self.drop1 = nn.Dropout(drop_probs[0])
+        # self.norm = norm_layer(hidden_features) if norm_layer is not None else nn.Identity()
+        # self.fc2 = linear_layer(hidden_features, out_features, bias=bias[1])
+        # self.drop2 = nn.Dropout(drop_probs[1])
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.act(x)
-        x = self.drop1(x)
-        x = self.norm(x)
-        x = self.fc2(x)
-        x = self.drop2(x)
+        # x = self.drop1(x)
+        # x = self.norm(x)
+        # x = self.fc2(x)
+        # x = self.drop2(x)
         return x
 
 @torch.no_grad
