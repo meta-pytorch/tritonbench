2a9eeb906bc8c571ba2b2a66836525397bd013d8
Xu Zhao <xzhao9@meta.com>
2025-11-12 13:21:10 -0800

    install helion

--- tools/helion/install.py
+++ tools/helion/install.py
@@ -21,12 +21,5 @@ def install_helion():
     subprocess.check_call(git_clone_cmd, cwd=HELION_INSTALL_PATH)
     git_checkout_cmd = ["git", "checkout", HEION_COMMIT]
     subprocess.check_call(git_checkout_cmd, cwd=HELION_PATH)
-    install_requirements_cmd = [
-        "pip",
-        "install",
-        "-r",
-        "requirements.txt",
-    ]
-    subprocess.check_call(install_requirements_cmd, cwd=HELION_PATH)
     install_helion_cmd = ["pip", "install", "-e", ".[dev]"]
     subprocess.check_call(install_helion_cmd, cwd=HELION_PATH)
