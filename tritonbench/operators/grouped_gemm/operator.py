b130719bdbfaebd882d14b13ce62cc4af4fa7823
Hongtao Yu <hoy@fb.com>
2025-12-02 11:24:49 -0800

    formatting

--- tritonbench/operators/grouped_gemm/operator.py
+++ tritonbench/operators/grouped_gemm/operator.py
@@ -52,7 +52,11 @@ if HAS_TLX:
     from .kernels import tlx_group_gemm_fn
 
 
-IS_B200 = is_cuda() and get_nvidia_gpu_model() in ("NVIDIA B200", "NVIDIA GB200", "NVIDIA GB300")
+IS_B200 = is_cuda() and get_nvidia_gpu_model() in (
+    "NVIDIA B200",
+    "NVIDIA GB200",
+    "NVIDIA GB300",
+)
 
 
 def get_default_shapes():
