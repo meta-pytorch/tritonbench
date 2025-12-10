582f2775bb30da1918e81950f4438029e2eec912
Xu Zhao <i@xuzhao.net>
2025-12-10 08:20:42 -0500

    fix quack install

--- tools/quack/install.py
+++ tools/quack/install.py
@@ -9,14 +9,9 @@ REPO_PATH = Path(os.path.abspath(__file__)).parent.parent.parent
 CURRENT_DIR = Path(os.path.abspath(__file__)).parent
 
 QUACK_REPO = "https://github.com/Dao-AILab/quack.git"
-QUACK_SHA = "bceb632dbac9bb0b55d48a7ed3ad204bd952fcb2"
+QUACK_SHA = "21f1c053b1c35a3aaf20002107d0704f364de10a"
 
 QUACK_INSTALL_PATH = REPO_PATH.joinpath(".install")
-
-
-def install_quack():
-    cmd = ["pip", "install", "-e", "."]
-    subprocess.check_call(cmd, cwd=QUACK_PATH)
 
 
 def install_quack():
@@ -28,5 +23,7 @@ def install_quack():
     subprocess.check_call(git_clone_cmd, cwd=QUACK_INSTALL_PATH)
     git_checkout_cmd = ["git", "checkout", QUACK_SHA]
     subprocess.check_call(git_checkout_cmd, cwd=quack_path)
-    install_helion_cmd = ["pip", "install", "-e", ".[dev]"]
-    subprocess.check_call(install_helion_cmd, cwd=quack_path)
+    install_quack_cmd = ["pip", "install", "--no-deps", "-e", ".[dev]"]
+    subprocess.check_call(install_quack_cmd, cwd=quack_path)
+    cmd = ["pip", "install", "nvidia-cutlass-dsl"]
+    subprocess.check_call(cmd, cwd=quack_path)
