feb4a1835b2d06de72e6f06e5fc856d446e8abc3
Xu Zhao <i@xuzhao.net>
2025-12-13 20:45:12 -0500

    fix install.sh

--- .ci/conda/install.sh
+++ .ci/conda/install.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/Miniconda3-latest-Linux-x86_64.sh
-
 sudo mkdir /workspace
 sudo chmod 777 /workspace
+
+wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/Miniconda3-latest-Linux-x86_64.sh
 
 cd /workspace
 chmod +x Miniconda3-latest-Linux-x86_64.sh
