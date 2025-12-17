729619de9b3c1d775fbe70baee1f39c21d522aee
Xu Zhao <xzhao9@meta.com>
2025-12-17 11:20:04 -0500

    fix transformer

--- tritonbench/operators/rope/operator.py
+++ tritonbench/operators/rope/operator.py
@@ -99,8 +99,11 @@ class Operator(BenchmarkOperator):
     def torch_compile_rotary_pos_emb_full_op(self, hidden_size, seq_length) -> Callable:
         q, k, cos, sin, pos_ids = self.prepare_input(hidden_size, seq_length)
         head_dim = hidden_size // self.num_q_heads
+        llama_config = LlamaConfig(
+            head_dim=head_dim,
+        )
         compiled = torch.compile(
-            LlamaRotaryEmbedding(head_dim, device=self.device),
+            LlamaRotaryEmbedding(llama_config, device=self.device),
             mode="max-autotune-no-cudagraphs",
         )
         cos, sin = compiled(k, pos_ids)
