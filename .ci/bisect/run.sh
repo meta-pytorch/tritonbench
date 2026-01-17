05da3fe119d43d748e649bd0051bc52125e0c0bf
Xu Zhao <xzhao9@fb.com>
2026-01-16 17:46:43 -0800

    add per commit log

--- .ci/bisect/run.sh
+++ .ci/bisect/run.sh
@@ -67,7 +67,7 @@ uv pip install -e .
 cd "${TRITONBENCH_DIR}"
 
 # kick off the bisect!
-USE_UV=1 CONDA_DIR="${WORKSPACE_DIR}/uv_venvs/${CONDA_ENV}" \
+PER_COMMIT_LOG=1 USE_UV=1 CONDA_DIR="${WORKSPACE_DIR}/uv_venvs/${CONDA_ENV}" \
 BASELINE_LOG="${PWD}/bisect_logs/baseline.log" \
 tritonparseoss bisect --triton-dir "${TRITON_SRC_DIR}" --test-script ./.ci/bisect/regression_detector.py \
     --good ${GOOD_COMMIT} --bad ${BAD_COMMIT}
