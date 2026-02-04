d31be15ee03f53c40e1266ed4faea90899e8e376
Xu Zhao <xzhao9@meta.com>
2026-02-04 11:04:21 -0500

    be in tlx benchmark

--- benchmarks/tlx/run.py
+++ benchmarks/tlx/run.py
@@ -18,40 +18,11 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
 from common import setup_tritonbench_cwd
 
 setup_tritonbench_cwd()
 
-from tritonbench.utils.env_utils import IS_BLACKWELL
-
-TLX_BENCHMARKS_BLACKWELL = {
-    "gemm": [
-        "tlx_matmul",
-        "tlx_matmul_ws",
-        "tlx_matmul_clc",
-        "tlx_matmul_pipelined",
-        "tlx_matmul_2cta",
-    ],
-}
-
-
-def gen_tlx_benchmarks():
-    acc: dict[Unknown, Unknown] = {}
-    for op in TLX_BENCHMARKS_BLACKWELL:
-        for backend in TLX_BENCHMARKS_BLACKWELL[op]:
-            if op not in acc:
-                acc[op] = {backend: {"tags": ["tlx"]}}
-            else:
-                acc[op].update(
-                    {
-                        backend: {
-                            "tags": ["tlx"],
-                        }
-                    }
-                )
-    return acc
-
+from tritonbench.utils.run_utils import run_benchmark_config_ci
 
 def gen_tlx_benchmark_config() -> Dict[str, Any]:
     from tlx_tutorial_plugin import load_tlx_tutorial_backends
@@ -71,8 +42,6 @@ def gen_tlx_benchmark_config() -> Dict[str, Any]:
 
     out = _load_benchmarks(os.path.join(CURRENT_DIR, "tlx_benchmarks.yaml"))
     tlx_tutorial_benchmark_metadata = load_tlx_tutorial_backends()
-    if IS_BLACKWELL:
-        tlx_tutorial_benchmark_metadata.update(gen_tlx_benchmarks())
     # only load benchmarks from runtime metadata
     metadata_benchmarks = get_benchmark_config_with_tags(
         tags=["tlx"],
@@ -98,14 +67,12 @@ def run():
     parser.add_argument(
         "--log-scuba", action="store_true", help="Upload results to Scuba."
     )
+    parser.add_argument(
+        "--output-dir", type=str, default=None, help="Output dir, default to .benchmark/tlx/run-<timestamp>"
+    )
     args = parser.parse_args()
-    setup_tritonbench_cwd()
-    from tritonbench.utils.run_utils import run_in_task, setup_output_dir
-    from tritonbench.utils.scuba_utils import decorate_benchmark_data, log_benchmark
 
-    run_timestamp, output_dir = setup_output_dir("tlx", ci=args.ci)
     # Run each operator
-    output_files = []
     tlx_benchmarks = gen_tlx_benchmark_config()
     print(yaml.dump(tlx_benchmarks))
     if args.generate_config:
@@ -113,43 +80,18 @@ def run():
             yaml.dump(tlx_benchmarks, f)
         logger.info(f"[tlx benchmark] Generated config file to {output_dir}.")
         return
-    for tlx_bench in tlx_benchmarks:
-        if args.op and f"--op {args.op}" not in tlx_benchmarks[tlx_bench]["args"]:
-            continue
-        op_args = tlx_benchmarks[tlx_bench]["args"].split(" ") + [
+    run_benchmark_config_ci(
+        args.name,
+        tlx_benchmarks,
+        extra_args=[
             "--plugin",
             "benchmarks.tlx.tlx_tutorial_plugin.load_tlx_tutorial_backends",
-        ]
-        output_file = output_dir.joinpath(f"{tlx_bench}.json")
-        op_args.extend(["--output-json", str(output_file.absolute())])
-        run_in_task(op_args=op_args, benchmark_name=tlx_bench)
-        # write pass or fail to result json
-        # todo: check every input shape has passed
-        output_file_name = Path(output_file).stem
-        if not os.path.exists(output_file) or os.path.getsize(output_file) == 0:
-            logger.warning(f"[tlx benchmark] Failed to run {output_file_name}.")
-            with open(output_file, "w") as f:
-                json.dump({f"tritonbench_{output_file_name}-pass": 0}, f)
-        else:
-            with open(output_file, "r") as f:
-                obj = json.load(f)
-            obj[f"tritonbench_{output_file_name}-pass"] = 1
-            with open(output_file, "w") as f:
-                json.dump(obj, f, indent=4)
-        output_files.append(output_file)
-    # Reduce all operator CSV outputs to a single output json
-    benchmark_data = [json.load(open(f, "r")) for f in output_files]
-    aggregated_obj = decorate_benchmark_data(
-        args.name, run_timestamp, args.ci, benchmark_data
+        ],
+        output_dir=args.output_dir,
+        op=args.op,
+        ci=args.ci,
+        log_scuba=args.log_scuba
     )
-    result_json_file = os.path.join(output_dir, "result.json")
-    with open(result_json_file, "w") as fp:
-        json.dump(aggregated_obj, fp, indent=4)
-    logger.info(f"[tlx benchmark] logging result json file to {result_json_file}.")
-    if args.log_scuba:
-        log_benchmark(aggregated_obj)
-        logger.info(f"[tlx benchmark] logging results to scuba.")
-
 
 if __name__ == "__main__":
     run()
