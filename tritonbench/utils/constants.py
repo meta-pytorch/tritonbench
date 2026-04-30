e4abb9f79e5afbd50aaefa865cda9017dbdfa4ce
Xu Zhao <i@xuzhao.net>
2026-04-30 01:03:17 -0400

    another fix

--- tritonbench/utils/constants.py
+++ tritonbench/utils/constants.py
@@ -1,9 +1,9 @@
 from typing import Dict, Tuple
 
 DEFAULT_WARMUP_REP_BY_ESTIMATED_KERNEL_MS: Dict[str, Tuple[int, int]] = {
-    "1": (100, 100),
-    "10": (100, 100),
-    "100": (100, 100),
+    "1": (25, 100),
+    "10": (25, 100),
+    "100": (25, 100),
 }
 DEFAULT_POWER_REPCNT = 2000
 DEFAULT_QUANTILES = [0.5, 0.1, 0.9]
